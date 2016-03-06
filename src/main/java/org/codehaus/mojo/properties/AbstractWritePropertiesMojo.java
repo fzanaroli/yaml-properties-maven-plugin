750ee262a67ff044678d082cb1cf99202f5da2ca
Roberto Trunfio <roberto.trunfio@zalando.de>
2016-03-06 11:06:10 +0100

    Preparing release 1.1.0 of yaml-properties plugin

--- src/main/java/org/codehaus/mojo/properties/AbstractWritePropertiesMojo.java
+++ src/main/java/org/codehaus/mojo/properties/AbstractWritePropertiesMojo.java
@@ -19,6 +19,11 @@ package org.codehaus.mojo.properties;
  * under the License.
  */
 
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -27,75 +32,60 @@ import java.util.Properties;
 
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file 
+ * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
  * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, 
+ *
+ * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY 
- * KIND, either express or implied.  See the License for the 
- * specific language governing permissions and limitations 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
  * under the License.
  */
-
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.project.MavenProject;
 
 /**
  * @author <a href="mailto:zarars@gmail.com">Zarar Siddiqi</a>
  * @version $Id$
  */
 public abstract class AbstractWritePropertiesMojo
-    extends AbstractMojo
-{
+        extends AbstractMojo {
 
-    @Parameter( defaultValue = "${project}", required = true, readonly = true )
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
     private MavenProject project;
 
-    @Parameter( required = true )
+    @Parameter(required = true)
     private File outputFile;
 
     /**
      * @param properties {@link Properties}
-     * @param file {@link File}
+     * @param file       {@link File}
      * @throws MojoExecutionException {@link MojoExecutionException}
      */
-    protected void writeProperties( Properties properties, File file )
-        throws MojoExecutionException
-    {
+    protected void writeProperties(Properties properties, File file)
+            throws MojoExecutionException {
         FileOutputStream fos = null;
-        try
-        {
-            fos = new FileOutputStream( file );
-            properties.store( fos, "Properties" );
-        }
-        catch ( FileNotFoundException e )
-        {
-            getLog().error( "Could not create FileOutputStream: " + fos );
-            throw new MojoExecutionException( e.getMessage(), e );
-        }
-        catch ( IOException e )
-        {
-            getLog().error( "Error writing properties: " + fos );
-            throw new MojoExecutionException( e.getMessage(), e );
+        try {
+            fos = new FileOutputStream(file);
+            properties.store(fos, "Properties");
+        } catch (FileNotFoundException e) {
+            getLog().error("Could not create FileOutputStream: " + fos);
+            throw new MojoExecutionException(e.getMessage(), e);
+        } catch (IOException e) {
+            getLog().error("Error writing properties: " + fos);
+            throw new MojoExecutionException(e.getMessage(), e);
         }
 
-        try
-        {
+        try {
             fos.close();
-        }
-        catch ( IOException e )
-        {
-            getLog().error( "Error closing FileOutputStream: " + fos );
-            throw new MojoExecutionException( e.getMessage(), e );
+        } catch (IOException e) {
+            getLog().error("Error closing FileOutputStream: " + fos);
+            throw new MojoExecutionException(e.getMessage(), e);
         }
     }
 
@@ -103,15 +93,12 @@ public abstract class AbstractWritePropertiesMojo
      * @throws MojoExecutionException {@link MojoExecutionException}
      */
     protected void validateOutputFile()
-        throws MojoExecutionException
-    {
-        if ( outputFile.isDirectory() )
-        {
-            throw new MojoExecutionException( "outputFile must be a file and not a directory" );
+            throws MojoExecutionException {
+        if (outputFile.isDirectory()) {
+            throw new MojoExecutionException("outputFile must be a file and not a directory");
         }
         // ensure path exists
-        if ( outputFile.getParentFile() != null )
-        {
+        if (outputFile.getParentFile() != null) {
             outputFile.getParentFile().mkdirs();
         }
     }
@@ -119,16 +106,14 @@ public abstract class AbstractWritePropertiesMojo
     /**
      * @return {@link MavenProject}
      */
-    public MavenProject getProject()
-    {
+    public MavenProject getProject() {
         return project;
     }
 
     /**
      * @return {@link #outputFile}
      */
-    public File getOutputFile()
-    {
+    public File getOutputFile() {
         return outputFile;
     }
 
--- src/main/java/org/codehaus/mojo/properties/ResourceType.java
+++ src/main/java/org/codehaus/mojo/properties/ResourceType.java
@@ -1,53 +1,46 @@
 package org.codehaus.mojo.properties;
 
+
 import java.util.HashSet;
 import java.util.Set;
 
-import edu.emory.mathcs.backport.java.util.Arrays;
+public enum ResourceType {
 
-public enum ResourceType 
-{
-
-    PROPERTIES( ".properties" ),
-    YAML( new String[] { ".yml", ".yaml" } );
+    PROPERTIES(".properties"),
+    YAML(".yml", ".yaml");
 
     private final Set<String> fileExtensions;
 
-    ResourceType( final String ... fileExtensions ) 
-    {
-        this.fileExtensions = new HashSet<String>( Arrays.asList( fileExtensions ) );
+    ResourceType(final String... fileExtensions) {
+        this.fileExtensions = new HashSet<String>();
+        for(final String fileExtension: fileExtensions){
+            this.fileExtensions.add(fileExtension);
+        }
     }
 
-    public Set<String> fileExtensions() 
-    {
-        return new HashSet<String>( fileExtensions );
-    }
-
-    public static Set<String> allFileExtensions( final ResourceType ... resourceTypes ) 
-    {
+    public static Set<String> allFileExtensions(final ResourceType... resourceTypes) {
         final Set<String> extensions = new HashSet<String>();
-        for ( final ResourceType resourceType : resourceTypes ) 
-	{
-            extensions.addAll( resourceType.fileExtensions() );
+        for (final ResourceType resourceType : resourceTypes) {
+            extensions.addAll(resourceType.fileExtensions());
         }
 
         return extensions;
     }
 
-    public static ResourceType getByFileName( final String fileName ) 
-    {
-        for ( final ResourceType resourceType : ResourceType.values() ) 
-	{
-            for ( final String extension : resourceType.fileExtensions() ) 
-	    {
-                if ( fileName.endsWith( extension ) ) 
-		{
+    public static ResourceType getByFileName(final String fileName) {
+        for (final ResourceType resourceType : ResourceType.values()) {
+            for (final String extension : resourceType.fileExtensions()) {
+                if (fileName.endsWith(extension)) {
                     return resourceType;
                 }
             }
         }
 
         return null;
+    }
+
+    public Set<String> fileExtensions() {
+        return new HashSet<String>(fileExtensions);
     }
 
 }
